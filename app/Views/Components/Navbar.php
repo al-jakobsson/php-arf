1d88caa5673452c1c902df87a35ca6168d20f2c4
Al Jakobsson <al.jakobsson@proton.me>
2024-02-29 09:37:50 +0100

    Arf!

--- app/Views/Components/Navbar.php
+++ app/Views/Components/Navbar.php
@@ -7,7 +7,7 @@ namespace Views\Components;
 <nav>
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="https://github.com/al-jakobsson/php-arf">About</a></li>
+        <li><a href="mailto:al.jakobsson@proton.me">Contact</a></li>
     </ul>
 </nav>
