96dca42f62529b20c835c4d91145a5981974b833
Al Jakobsson <al.jakobsson@proton.me>
2024-02-29 09:33:01 +0100

    Arf!

--- app/Views/HomePage.php
+++ app/Views/HomePage.php
@@ -12,9 +12,10 @@ use Arf\Safe;
 <?php View::render('Components/DefaultPageHeader', ['title' => 'Welcome!']); ?>
 <?php View::render('Components/Navbar'); ?>
 
-<div class="centered">
+<main>
     <h1><?= Safe::html($heading ?? 'Home') ?></h1>
+    <img src="/images/arf-logo-3.png" alt="Arf logo" height="300px">
     <p>Welcome to your first Arf page.</p>
-</div>
+</main>
 
 <?php View::render('Components/DefaultPageFooter'); ?>
